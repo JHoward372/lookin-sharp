0119bba519539ec0111e6b59c838c8fc54ca30e5
JHoward372 <jhoward@icstars.org>
2022-11-08 16:12:44 -0600

    Changed Homeboy's value from "Godfrey" to "Devon"

--- LookinSharp/LookinSharp/Program.cs
+++ LookinSharp/LookinSharp/Program.cs
@@ -12,7 +12,7 @@ namespace LookinSharp // Organizes code; container for classes and other namespa
             Console.WriteLine("Hey there! Gahdamn, you looking real sharp!");
             // Without the using System line, you'd have to write System.Console.WriteLine() for this to work
 
-            string Homeboy = "Godfrey"; // Declaring a variable
+            string Homeboy = "Devon"; // Declaring a variable
             Console.WriteLine(Homeboy); // Calling a declared variable
         }
     }
