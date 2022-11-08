dc86ba0eb71aad3b5e2be4884c8be2c026f7ba0f
JHoward372 <jhoward@icstars.org>
2022-11-08 12:47:34 -0600

    Project Setup
    Created template for project

---
+++ LookinSharp/LookinSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LookinSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey there! Gahdamn, you looking real sharp!");
+        }
+    }
+}
+
